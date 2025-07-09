9b19495801cc776c17fbd12acc88b7bf557e1eea
Lonahasel <163159838+lonahaselbiswal@users.noreply.github.com>
2025-07-09 16:50:37 +0530

    Mandatory Hands-Ons i.e Week_3

---
+++ WEEK_3/Configuring a Basic Spring Application/BookRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+public class BookRepository {
+    public void fetchBooks() {
+        System.out.println("Fetching books from the database...");
+    }
+}
+
---
+++ WEEK_3/Configuring a Basic Spring Application/BookService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import com.example.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        System.out.println("BookService: Displaying books...");
+        bookRepository.fetchBooks();
+    }
+}
